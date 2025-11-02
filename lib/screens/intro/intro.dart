250e4d8d7b3e30e65ec7157105ad68731321f7bf
sungyeon6 <0504sypark@naver.com>
2025-11-03 00:39:32 +0900

    [FEAT] 인트로 화면

---
+++ lib/screens/intro/intro.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Intro extends StatefulWidget {
+  const Intro({super.key});
+
+  @override
+  State<Intro> createState() => _IntroState();
+}
+
+class _IntroState extends State<Intro> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset('assets/images/polygon.png'),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 50),
+                Text(
+                  'Art\n+\nStory',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Pretendard-Regular',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  '알토리',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontFamily: 'Pretendard-Bold',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  '예술과 기록이 만나다',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Pretendard-Regular',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// TODO: 애니메이션 효과

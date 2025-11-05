3e3077a3da6abffcb19a006ee12c1e83d2fe02eb
sungyeon6 <0504sypark@naver.com>
2025-11-06 01:54:19 +0900

    [FEAT] 전시회 정보 overlay, 작품 상세 페이지 연결

--- lib/screens/exhibition/exhibition_detail.dart
+++ lib/screens/exhibition/exhibition_detail.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
 
+class ExhibitionDetail extends StatefulWidget {
+  final int index;
+  const ExhibitionDetail({super.key, required this.index});
+
+  @override
+  State<ExhibitionDetail> createState() => _ExhibitionDetailState();
+}
+
+class _ExhibitionDetailState extends State<ExhibitionDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('작품 상세 페이지')
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/exhibition/exhibition_main.dart
+++ lib/screens/exhibition/exhibition_main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../data/exhibition_loader.dart';
 import '../../models/exhibition.dart';
+import 'exhibition_detail.dart';
 
 class ExhibitionMain extends StatefulWidget {
   const ExhibitionMain({super.key});
@@ -12,6 +14,7 @@ class ExhibitionMain extends StatefulWidget {
 
 class _ExhibitionMainState extends State<ExhibitionMain> {
   late Future<List<Exhibition>> exhibitionsFuture;
+  final Set<int> _shownIndex = {};
 
   @override
   void initState() {
@@ -19,12 +22,37 @@ class _ExhibitionMainState extends State<ExhibitionMain> {
     exhibitionsFuture = loadExhibitions();
   }
 
+  void _handleExhb(int index) {
+    setState(() {
+      if (_shownIndex.contains(index)) {
+        _shownIndex.remove(index);
+        Get.to(() => ExhibitionDetail(index: index));
+      } else {
+        setState(() {
+          _shownIndex.add(index);
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('내가 감상한 전시회에요!'),
-
+        automaticallyImplyLeading: false,
+        title: Text(
+          '내가 감상한 전시회에요!',
+          style: TextStyle(
+            fontFamily: 'Pretendard-SemiBold',
+            fontSize: 20,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {}, // TODO: 좋아요 페이지 연결
+            icon: Icon(Icons.favorite), color: Color(0xff0D9F34)
+          ),
+        ],
       ),
       body: FutureBuilder<List<Exhibition>>(
         future: exhibitionsFuture,
@@ -34,13 +62,28 @@ class _ExhibitionMainState extends State<ExhibitionMain> {
           }
 
           final exhibitions = snapshot.data!;
-          return ListView.builder(
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 4,
+            ),
             itemCount: exhibitions.length,
             itemBuilder: (context, index) {
               final exhb = exhibitions[index];
-              return ListTile(
-                leading: Image.asset(exhb.imagePath, width: 150, fit: BoxFit.cover),
-                title: Text(exhb.name),
+              final showText = _shownIndex.contains(index);
+
+              return GestureDetector(
+                onTap: () => _handleExhb(index),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.asset(exhb.imagePath, width: 150, height: 200, fit: BoxFit.cover),
+                    if (showText)
+                      Container(
+                        child: Text(exhb.name),
+                      ),
+                  ],
+                ),
               );
             },
           );

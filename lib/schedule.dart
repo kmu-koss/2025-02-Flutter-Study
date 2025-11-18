fd66fc171b5903cbf61036cd6ff10a7a61e1730b
HALF1313\bkt04 <bkt0403@naver.com>
2025-11-18 11:08:46 +0900

    commit

--- lib/schedule.dart
+++ lib/schedule.dart
@@ -1,8 +1,90 @@
 import 'package:flutter/material.dart';
 import 'plusSchedule.dart';
 
+List week = ['월', '화', '수', '목', '금'];
+var kColumnLength = 22;
+double kFirstColumnHeight = 20;
+double kBoxSize = 52;
+
 class SchedulePage extends StatelessWidget {
   const  SchedulePage({super.key});
+
+  Widget buildTimeColumn() {
+    return Expanded(
+        child: Column(
+          children: [
+            SizedBox(
+              height: kFirstColumnHeight,
+            ),
+            ...List.generate(
+              kColumnLength,
+                (index) {
+                if (index % 2 == 0) {
+                  return const Divider(
+                    color: Colors.grey,
+                    height: 0,
+                  );
+                }
+                return SizedBox(
+                  height:  kBoxSize,
+                  child: Center(child: Text('${index ~/ 2 + 9}')),
+                );
+                },
+            ),
+          ],
+        ),
+    );
+  }
+
+  List<Widget> buildDayColumn(int index) {
+    return [
+      const VerticalDivider(
+        color: Colors.grey,
+        width: 0,
+      ),
+      Expanded(
+        flex: 4,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                  child: Text(
+                    '${week[index]}',
+                  ),
+                ),
+                ...List.generate(
+                  kColumnLength,
+                    (index) {
+                    if (index % 2 == 0) {
+                      return const Divider(
+                        color: Colors.grey,
+                        height: 0,
+                      );
+                    }
+                    return SizedBox(
+                      height: kBoxSize,
+                      child: Container(),
+                    );
+                    },
+                ),
+              ],
+            ),
+            if (index==1)
+            Positioned(
+              child: Container(
+                color: Color(0xffB8D8E6),
+              ),
+              top: kFirstColumnHeight + kBoxSize / 2,
+              height: kBoxSize + kBoxSize * 0.5,
+              width: 100,
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,10 +112,21 @@ class SchedulePage extends StatelessWidget {
         ],
       ),
       backgroundColor: Color(0xffFDF7EF),
-      body: const Center(
-        child: Text(
-          '시간표',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Container(
+        height: kColumnLength / 2 * kBoxSize + kColumnLength,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            buildTimeColumn(),
+            ...buildDayColumn(0),
+            ...buildDayColumn(1),
+            ...buildDayColumn(2),
+            ...buildDayColumn(3),
+            ...buildDayColumn(4),
+          ],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(backgroundColor: Color(0xffB8D8E6),
